0736a2e5b1fc8a10ffb0931b4d0e1c8f344745d0
rudolf <rudolf.degeijter@meemoo.be>
2022-05-06 09:28:54 +0200

    Make pids random and the correct length. Also ran `cargo fmt`.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web, Responder, Result, App, HttpServer};
+use actix_web::{get, web, App, HttpServer, Responder, Result};
+use rand::prelude::*;
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -8,17 +9,22 @@ struct Pid {
 
 #[get("/pid")]
 async fn pid() -> Result<impl Responder> {
-    let pid = Pid{id: String::from("1")};
+    let pid = Pid {
+        id: rand::thread_rng()
+            .sample_iter(&rand::distributions::Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect::<String>()
+            .to_ascii_lowercase(),
+    };
     let pids = vec![pid];
     Ok(web::Json(pids))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(pid)
-    })
-    .bind(("127.0.0.1", 9090))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(pid))
+        .bind(("127.0.0.1", 9090))?
+        .run()
+        .await
 }
